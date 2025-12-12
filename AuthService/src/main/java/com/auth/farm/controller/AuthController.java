1b79bbd3ce9f6524d8dae4124406bddcecef9d3f
Chinnolla Koteshwar <chinnollakoteshwar@gmail.com>
2025-12-12 15:41:10 +0530

    farm_auth

--- AuthService/src/main/java/com/auth/farm/controller/AuthController.java
+++ AuthService/src/main/java/com/auth/farm/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/form_auth")
 @RequiredArgsConstructor
 public class AuthController {
 
